eba9ef155e6b8c8b8a9996c27bb870baacbd1d42
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-04-08 09:44:40 -0600

    Custom hook agregado, vuamos

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
+import React from 'react';
 import Confetti from 'react-confetti';
 import { HeaderTodo } from './components/HeaderTodo/HeaderTodo.jsx';
-import { TaskComponent } from './components/TasksComponent/TasksComponent.jsx'
+import { TaskComponent } from './components/TasksComponent/TasksComponent.jsx';
 import { NewTask } from './components/NewTask/NewTask.jsx';
-import {Task} from './components/Task/Task.jsx'
-// const TaskFake = [
-//    {text:'Cortar cebolla',completed:true},{text:'Tomar el Curso de Intro a React.js',completed:false},{text:'Llorar con la Llorona',completed:false},{text:'LALALALALA',completed:false}
-// ]
+import { Task } from './components/Task/Task.jsx';
+
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = React.useState(parsedItem);
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+  return [item, saveItem];
+}
 
 function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-  if(!localStorageTodos) {
-    localStorage.setItem('TODOS_V1',JSON.stringify([]));
-    parsedTodos=[];
-  } else { 
-    parsedTodos=JSON.parse(localStorageTodos);
-  }
-  const[todos,setTodos]=React.useState(parsedTodos);
-  const[searchValue,setSearchValue]=React.useState('')
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
   const [showConfetti, setShowConfetti] = React.useState(false);
-  const saveTodos=(newTodos)=>{localStorage.setItem('TODOS_V1',
-  JSON.stringify(newTodos));
-  setTodos(newTodos);}
 
   const completedTasks = todos.filter((task) => !!task.completed).length;
   const totalTasks = todos.length;
   const allTasksCompleted = todos.length > 0 && todos.every((todo) => todo.completed);
+
   React.useEffect(() => {
     if (allTasksCompleted) {
       setShowConfetti(true);
-      const timer = setTimeout(() => setShowConfetti(false), 5000);
-      return () => clearTimeout(timer);
+      // const timer = setTimeout(() => setShowConfetti(false), 8000);
+      // return () => clearTimeout(timer);
     }
   }, [allTasksCompleted]);
 
@@ -39,42 +44,45 @@ function App() {
     const taskText = task.text.toLowerCase();
     const searchText = searchValue.toLowerCase();
     return taskText.includes(searchText);
-  })
+  });
 
-  const completeTodo=(text)=>{
-    const newTodos=[...todos];
-    const todoIndex=newTodos.findIndex((todo)=>todo.text==text);
-    newTodos[todoIndex].completed=true;
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    newTodos[todoIndex].completed = true;
     saveTodos(newTodos);
   };
-  const deleteTodo=(text)=>{
-    const newTodos=[...todos];
-    const todoIndex=newTodos.findIndex((todo)=>todo.text==text);
-    newTodos.splice(todoIndex,1);
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    newTodos.splice(todoIndex, 1);
     saveTodos(newTodos);
-  }
+  };
+
   return (
     <>
-      {showConfetti && <Confetti/>}
-      <HeaderTodo 
-      completed={completedTasks} 
-      total={totalTasks} 
-      searchValue={searchValue} 
-      setSearchValue={setSearchValue}/>
+      {showConfetti && <Confetti numberOfPieces={1550}  tweenDuration={5000} recycle={false}/>}
+      <HeaderTodo
+        completed={completedTasks}
+        total={totalTasks}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TaskComponent>
-        {filteredTasks.map((task) => {
-          return <Task 
-              key={task.text}
-              text={task.text}
-              completed={task.completed}
-              onComplete={() => completeTodo(task.text)}
-              onDelete={() => deleteTodo(task.text)}
-              />
-          })}
+        {filteredTasks.map((task) => (
+          <Task
+            key={task.text}
+            text={task.text}
+            completed={task.completed}
+            onComplete={() => completeTodo(task.text)}
+            onDelete={() => deleteTodo(task.text)}
+          />
+        ))}
       </TaskComponent>
-      <NewTask/>
+      <NewTask />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
